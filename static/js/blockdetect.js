b7aa8a9a5ecf9c1728d45bb208d407472af0b99a
Finbae <16717245+Finbae@users.noreply.github.com>
2020-03-22 13:42:16 +0000

    Moved to bonkleagues.io

--- static/js/blockdetect.js
+++
@@ -1,9 +0,0 @@
-//Adblock detection
-function adBlockDetected() {
-	$('#adbnote').show();
-}
-if(typeof blockAdBlock === 'undefined') {
-	adBlockDetected();
-} else {
-	blockAdBlock.onDetected(adBlockDetected);
-}
